3c2b86f10adb5063c2de72ee3f415efec4a03a65
Ting Hao <ztingh@gmail.com>
2021-05-21 18:58:30 +0800

    feat: add questions and tags component

---
+++ src/questionSearch/Tags.tsx
@@ -0,0 +1,32 @@
+import { Box, Button, SimpleGrid } from "@chakra-ui/react";
+
+interface ITagsProps {
+  handleTagOnClick: (name: string) => void;
+  curTag: string;
+  tags: any[];
+}
+
+const Tags = (props: ITagsProps) => {
+  const { tags, curTag, handleTagOnClick } = props;
+
+  return (
+    <Box mt="5px">
+      <SimpleGrid minChildWidth="100px" spacing="10px">
+        {tags.map((tag) => (
+          <Button
+            d="inline-block"
+            fontSize="12px"
+            cursor="pointer"
+            key={tag.name}
+            colorScheme={curTag === tag.name ? "blue" : undefined}
+            onClick={() => handleTagOnClick(tag.name)}
+          >
+            {tag.name}
+          </Button>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default Tags;
